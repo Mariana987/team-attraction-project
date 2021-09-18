a0235271175ba689c61c1bab690965ac3ff54770
NatalliaMaskalenko <MoskNatulya@gmail.com>
2021-09-18 02:11:09 +0200

    CСохраняет  ID события в локал-сторедж

--- src/js/button_more.js
+++ src/js/button_more.js
@@ -1,83 +1,68 @@
 import cardSetTemplateHBS from '../templates/set-of-cards.hbs';
+import refs from './refs';
+import renderingCardSet from './renderingСardSet';
+import getEventID from './open-close-modal';
+import getEventsByAttractions from './events-api';
 // import { getEventsByOptions, getEventsByAttractions } from '../js/events-api';
-import { pagination } from './pagination';
+// import { pagination } from './pagination';
 
-// информация из events-api
 
-// const API_KEY = 'GcvUr561HaBI30kU58PhKSa9RWqvwjKx';
-// const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/';
-// const breakPoint = 'events.json';
+refs.backdropRef.addEventListener('click', onbuttonMoreElClick);
 
-// function getEventsByAttractions(id, page = false) {
-//   if (!id) return;
-//   page = page ? `&page=${page}` : '';
-//   const url = `${BASE_URL}${breakPoint}?apikey=${API_KEY}&locale=*&attraction=LP`;
-//   return fetchJSON(url).then(res => getPage(res));
+function onbuttonMoreElClick(event) {
+    const authorID = sessionStorage.getItem('authorID');
+    console.log(authorID);
+     let action = event.target.dataset.action;
+    if (action) {
+         refs.backdropRef.classList.remove('open');
+         fetchAndMarkUp();
+    }
+};
+
+
+function onInput() {
+  const keyword = refs.keywordInput.value;
+  const country = refs.countryInput.value;
+  if (keyword === '') {
+    error({
+      text: 'Please enter something!',
+      delay: 2000,
+    });
+  } else {
+    renderingCardSet(country, keyword);
+  }
+}
+
+function rendering(arr) {
+  const cardSetTemplateAction = cardSetTemplateHBS(arr.cards);
+  refs.cardSetContainer.innerHTML = cardSetTemplateAction;
+
+
+  // функция берет ID ивента и посылает запрос на сервер. Функция временная так как костыль))
+//   getEventID();
+  // 
+  // 
+  searchCardsLinks(); //???
+
+}
+
+//  function renderingCardSet(country, keyword, page = null) {
+//   // console.log(pagination._currentPage);
+//   getEventsByOptions(country, keyword, page)
+//     .then(res => {
+//       // console.log(res);
+//       const totalPages = res.totalPages > 45 ? 45 : res.totalPages;
+//       pagination._options.totalItems = totalPages;
+//       pagination._paginate(res.number);
+//       return res;
+//     })
+//     .then(rendering)
+//     .catch(err =>
+//       error({
+//         text: err,
+//         delay: 3000,
+//       }),
+//     );
 // }
 
-const attractions = 'LP'
 
-function getEventsByAttractions() {
-  
-  const url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=GcvUr561HaBI30kU58PhKSa9RWqvwjKx&locale=*&keyword=${attractions}`;
-  return fetchJSON(url).then(res => getPage(res));
-}
-
-function fetchJSON(url) {
-  return fetch(url).then(res => res.json());
-}
-
-function getPage(data) {
-//   if (obj.page.totalPages === 0) {
-//     throw 'Nothing found from these search criteria';
-//   }
-//   const arrCards = obj?._embedded?.events?.map(item => {
-//     return {
-//       id: item?.id,
-//       name: item?.name,
-//       date: item?.dates?.start?.localDate,
-//       promoter: item?.promoter?.name,
-//       venues: item?._embedded?.venues[0]?.name,
-//       images: item?.images,
-//     };
-//   });
-//   return {
-//     number: obj?.page?.number,
-//     size: obj?.page?.size,
-//     totalElements: obj?.page?.totalElements,
-//     totalPages: obj?.page?.totalPages,
-//     cards: arrCards,
-//   };
-    return data._embedded;
-}
-
-
-// информация из events-api
-
-function fetchAndMarkUp() {
-   return getEventsByAttractions().then(appendArtiklesMarkup);
-
-};
-
-function appendArtiklesMarkup(hits) {
-    cardSetContainerEl.insertAdjacentHTML('beforeend', cardSetTemplateHBS(hits));
-};
-
-
-
-const backdropModalEl = document.querySelector('[data-modal-backdrop]');
-const buttonMoreEl = document.querySelector('.modal__btn-more');
-const cardSetContainerEl = document.querySelector('#js-set-of-cards');
-buttonMoreEl.addEventListener('click', onbuttonMoreElClick);
-
-function onbuttonMoreElClick(evt) {
-    backdropModalEl.classList.remove('open');
-    fetchAndMarkUp();
-    // rendering(arrCards);
-};
-
-// function rendering(arr) {
-//   const cardSetTemplateAction = cardSetTemplateHBS(arr.cards);
-//   refs.cardSetContainer.innerHTML = cardSetTemplateAction;
-//   searchCardsLinks(); //???
-// }

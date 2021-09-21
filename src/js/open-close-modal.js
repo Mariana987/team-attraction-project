98c44a3016e847fb6f7057339e0c8821b0803d3b
Viacheslav-ua <atph@ukr.net>
2021-09-21 07:55:18 +0300

    add modal syze

--- src/js/open-close-modal.js
+++ src/js/open-close-modal.js
@@ -48,6 +48,12 @@ function getEventID() {
 
 function renderingModal(arr) {
   const modalContentTemplateAction = modalContentTemplateHBS(arr);
+
+  // -----------------Высота модалки при ее загрузке------------------------------->
+  const windowHeight = window.innerHeight; // Получаем высоту вьюпорта
+  refs.modalWindow.style.height = `${windowHeight - 40}px`; //Задаем высоту модалки
+  // ------------------------------------------------------------------------------
+
   refs.modalWindow.innerHTML = modalContentTemplateAction;
   console.log(arr.who);
   localStorage.setItem('author', JSON.stringify(arr.who));
